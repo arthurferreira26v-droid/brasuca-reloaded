6c7ff6cfeb5d458e9538c66202a70dc5adfefc6e
arthurferreira26v-droid <arthurferreira26.v@gmail.com>
2026-01-12 03:44:37 -0300

    nova atu

--- src/data/teams.ts
+++ src/data/teams.ts
@@ -1,24 +1,3 @@
-import realMadridLogo from "@/assets/teams/real-madrid.png";
-import barcelonaLogo from "@/assets/teams/barcelona.png";
-import manCityLogo from "@/assets/teams/man-city.png";
-import liverpoolLogo from "@/assets/teams/liverpool.png";
-import psgLogo from "@/assets/teams/psg.png";
-import vascoLogo from "@/assets/teams/vasco.png";
-import santosLogo from "@/assets/teams/santos.png";
-import flamengoLogo from "@/assets/teams/flamengo.png";
-import corinthiansLogo from "@/assets/teams/corinthians.png";
-import fluminenseLogo from "@/assets/teams/fluminense.png";
-import gremioLogo from "@/assets/teams/gremio.svg";
-import atleticoMgLogo from "@/assets/teams/atletico-mg.png";
-import remoLogo from "@/assets/teams/remo.png";
-import mirassolLogo from "@/assets/teams/mirassol.png";
-import bahiaLogo from "@/assets/teams/bahia.png";
-import bragantinoLogo from "@/assets/teams/bragantino.png";
-import vitoriaLogo from "@/assets/teams/vitoria.png";
-import athleticoPrLogo from "@/assets/teams/athletico-pr.svg";
-import coritibalogo from "@/assets/teams/coritiba.png";
-import chapecoenselogo from "@assets/teams/chapecoense.png";
-
 export interface Team {
   id: string;
   name: string;
@@ -28,45 +7,56 @@ export interface Team {
 }
 
 export const teams: Team[] = [
-  // Brasileiro (20 times)
-  { id: "flamengo", name: "Flamengo", league: "brasileiro", rating: 5, logo: flamengoLogo },
-  { id: "palmeiras", name: "Palmeiras", league: "brasileiro", rating: 5, logo: "https://upload.wikimedia.org/wikipedia/commons/1/10/Palmeiras_logo.svg" },
-  { id: "santos", name: "Santos", league: "brasileiro", rating: 4, logo: santosLogo },
-  { id: "sao-paulo", name: "São Paulo", league: "brasileiro", rating: 4, logo: "https://upload.wikimedia.org/wikipedia/commons/6/6f/Brasao_do_Sao_Paulo_Futebol_Clube.svg" },
-  { id: "corinthians", name: "Corinthians", league: "brasileiro", rating: 4, logo: corinthiansLogo },
-  { id: "gremio", name: "Grêmio", league: "brasileiro", rating: 4, logo: gremioLogo },
-  { id: "atletico-mg", name: "Atlético Mineiro", league: "brasileiro", rating: 4, logo: atleticoMgLogo },
-  { id: "internacional", name: "Internacional", league: "brasileiro", rating: 3, logo: "https://upload.wikimedia.org/wikipedia/commons/f/f1/Escudo_do_Sport_Club_Internacional.svg" },
-  { id: "botafogo", name: "Botafogo", league: "brasileiro", rating: 3, logo: "https://upload.wikimedia.org/wikipedia/commons/5/52/Botafogo_de_Futebol_e_Regatas_logo.svg" },
-  { id: "vasco", name: "Vasco da Gama", league: "brasileiro", rating: 3, logo: vascoLogo },
-  { id: "fluminense", name: "Fluminense", league: "brasileiro", rating: 3, logo: fluminenseLogo },
-  { id: "cruzeiro", name: "Cruzeiro", league: "brasileiro", rating: 3, logo: "https://upload.wikimedia.org/wikipedia/commons/9/90/Cruzeiro_Esporte_Clube_%28logo%29.svg" },
-  // Novos times brasileiros
-  { id: "athletico-pr", name: "Atlético Paranaense", league: "brasileiro", rating: 4, logo: athleticoPrLogo },
-  { id: "bahia", name: "Bahia", league: "brasileiro", rating: 3, logo: bahiaLogo },
-  { id: "bragantino", name: "Bragantino", league: "brasileiro", rating: 3, logo: bragantinoLogo },
-  { id: "vitoria", name: "Vitória", league: "brasileiro", rating: 2, logo: vitoriaLogo },
-  { id: "coritiba", name: "Coritiba", league: "brasileiro", rating: 2, logo: "https://upload.wikimedia.org/wikipedia/commons/c/c9/Coritiba_FBC_-_parana.svg" },
-  { id: "chapecoense", name: "Chapecoense", league: "brasileiro", rating: 2, logo: "https://upload.wikimedia.org/wikipedia/commons/e/ef/Associacao_Chapecoense_de_Futebol_logo.svg" },
-  { id: "mirassol", name: "Mirassol", league: "brasileiro", rating: 2, logo: mirassolLogo },
-  { id: "remo", name: "Remo", league: "brasileiro", rating: 2, logo: remoLogo },
+  // 🇧🇷 Brasileiro
+  { id: "flamengo", name: "Flamengo", league: "brasileiro", rating: 5, logo: "/teams/flamengo.png" },
+  { id: "palmeiras", name: "Palmeiras", league: "brasileiro", rating: 5, logo: "/teams/palmeiras.svg" },
+  { id: "santos", name: "Santos", league: "brasileiro", rating: 4, logo: "/teams/santos.png" },
+  { id: "sao-paulo", name: "São Paulo", league: "brasileiro", rating: 4, logo: "/teams/sao-paulo.svg" },
+  { id: "corinthians", name: "Corinthians", league: "brasileiro", rating: 4, logo: "/teams/corinthians.png" },
+  { id: "gremio", name: "Grêmio", league: "brasileiro", rating: 4, logo: "/teams/gremio.svg" },
+  { id: "atletico-mg", name: "Atlético Mineiro", league: "brasileiro", rating: 4, logo: "/teams/atletico-mg.png" },
+  { id: "internacional", name: "Internacional", league: "brasileiro", rating: 3, logo: "/teams/internacional.svg" },
+  { id: "botafogo", name: "Botafogo", league: "brasileiro", rating: 3, logo: "/teams/botafogo.svg" },
+  { id: "vasco", name: "Vasco da Gama", league: "brasileiro", rating: 3, logo: "/teams/vasco.png" },
+  { id: "fluminense", name: "Fluminense", league: "brasileiro", rating: 3, logo: "/teams/fluminense.png" },
+  { id: "cruzeiro", name: "Cruzeiro", league: "brasileiro", rating: 3, logo: "/teams/cruzeiro.svg" },
 
-  // Europeu
-  { id: "real-madrid", name: "Real Madrid", league: "europeu", rating: 5, logo: realMadridLogo },
-  { id: "barcelona", name: "Barcelona", league: "europeu", rating: 5, logo: barcelonaLogo },
-  { id: "man-city", name: "Manchester City", league: "europeu", rating: 5, logo: manCityLogo },
-  { id: "liverpool", name: "Liverpool", league: "europeu", rating: 4, logo: liverpoolLogo },
-  { id: "bayern", name: "Bayern Munich", league: "europeu", rating: 5, logo: "https://upload.wikimedia.org/wikipedia/commons/1/1b/FC_Bayern_M%C3%BCnchen_logo_%282017%29.svg" },
-  { id: "psg", name: "Paris Saint-Germain", league: "europeu", rating: 4, logo: psgLogo },
-  { id: "juventus", name: "Juventus", league: "europeu", rating: 4, logo: "https://upload.wikimedia.org/wikipedia/commons/a/a8/Juventus_FC_-_pictogram_black_%28Italy%2C_2017%29.svg" },
-  { id: "milan", name: "AC Milan", league: "europeu", rating: 4, logo: "https://upload.wikimedia.org/wikipedia/commons/d/d0/Logo_of_AC_Milan.svg" },
-  { id: "chelsea", name: "Chelsea", league: "europeu", rating: 4, logo: "https://upload.wikimedia.org/wikipedia/commons/c/cc/Chelsea_FC.svg" },
-  { id: "arsenal", name: "Arsenal", league: "europeu", rating: 4, logo: "https://upload.wikimedia.org/wikipedia/commons/5/53/Arsenal_FC.svg" },
-  { id: "inter", name: "Inter Milan", league: "europeu", rating: 4, logo: "https://upload.wikimedia.org/wikipedia/commons/0/05/FC_Internazionale_Milano_2021.svg" },
-  { id: "atletico", name: "Atlético Madrid", league: "europeu", rating: 4, logo: "https://upload.wikimedia.org/wikipedia/commons/f/f4/Atletico_Madrid_2017_logo.svg" },
+  // ➕ Brasileiros adicionais
+  { id: "athletico-pr", name: "Athletico Paranaense", league: "brasileiro", rating: 4, logo: "/teams/athletico-pr.svg" },
+  { id: "bahia", name: "Bahia", league: "brasileiro", rating: 3, logo: "/teams/bahia.png" },
+  { id: "bragantino", name: "Bragantino", league: "brasileiro", rating: 3, logo: "/teams/bragantino.png" },
+  { id: "vitoria", name: "Vitória", league: "brasileiro", rating: 2, logo: "/teams/vitoria.png" },
+  { id: "coritiba", name: "Coritiba", league: "brasileiro", rating: 2, logo: "/teams/coritiba.png" },
+  { id: "chapecoense", name: "Chapecoense", league: "brasileiro", rating: 2, logo: "/teams/chapecoense.png" },
+  { id: "mirassol", name: "Mirassol", league: "brasileiro", rating: 2, logo: "/teams/mirassol.png" },
+  { id: "remo", name: "Remo", league: "brasileiro", rating: 2, logo: "/teams/remo.png" },
+
+  // 🇪🇺 Europeu
+  { id: "real-madrid", name: "Real Madrid", league: "europeu", rating: 5, logo: "/teams/real-madrid.png" },
+  { id: "barcelona", name: "Barcelona", league: "europeu", rating: 5, logo: "/teams/barcelona.png" },
+  { id: "man-city", name: "Manchester City", league: "europeu", rating: 5, logo: "/teams/man-city.png" },
+  { id: "liverpool", name: "Liverpool", league: "europeu", rating: 4, logo: "/teams/liverpool.png" },
+  { id: "bayern", name: "Bayern Munich", league: "europeu", rating: 5, logo: "/teams/bayern.svg" },
+  { id: "psg", name: "Paris Saint-Germain", league: "europeu", rating: 4, logo: "/teams/psg.png" },
+  { id: "juventus", name: "Juventus", league: "europeu", rating: 4, logo: "/teams/juventus.svg" },
+  { id: "milan", name: "AC Milan", league: "europeu", rating: 4, logo: "/teams/milan.svg" },
+  { id: "chelsea", name: "Chelsea", league: "europeu", rating: 4, logo: "/teams/chelsea.svg" },
+  { id: "arsenal", name: "Arsenal", league: "europeu", rating: 4, logo: "/teams/arsenal.svg" },
+  { id: "inter", name: "Inter Milan", league: "europeu", rating: 4, logo: "/teams/inter.svg" },
+  { id: "atletico", name: "Atlético Madrid", league: "europeu", rating: 4, logo: "/teams/atletico-madrid.svg" },
 ];
 
 export const leagues = [
   { id: "brasileiro", name: "Brasileiro", flag: "🇧🇷" },
   { id: "europeu", name: "Europeu", flag: "🇪🇺" },
 ];
+
+
+
+
+
+
+
+
+
+
